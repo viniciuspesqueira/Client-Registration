71c98b4e077748d6fa4430e615039ea01564fc98
Vinicius Pesqueira <vinicius.pesqueira31@gmail.com>
2025-11-30 16:54:24 -0300

    helmet added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // dotenv lib added
-require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -19,6 +19,7 @@ app.use(logger('dev'));
 app.use(express.urlencoded({ extended:true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- db.js
+++ db.js
@@ -1,61 +1,67 @@
-const mysql = require('mysql2/promise');
+require("dotenv").config()
+const mysql = require("mysql2/promise")
 
 async function connect() {
-    if(global.connection && global.connection.state !== 'disconnected')
-        return global.connection;
-    const connection = await mysql.createConnection({
-        host :'localhost',
-        port : 3306,
-        // database user
-        user : process.env.user,
-        // user password
-        password: process.env.password,
-        database: 'crud'
-    })
+  if (global.connection && global.connection.state !== "disconnected")
+    return global.connection
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    // database user
+    user: process.env.user,
+    // user password
+    password: process.env.password,
+    database: "crud",
+  })
 
-    console.log("Conectou no MySQL!");
-    global.connection = connection;
-    return global.connection;
+  console.log("Conectou no MySQL!")
+  global.connection = connection
+  return global.connection
 }
 
-connect();
+connect()
 
 async function selectClients() {
-    const conn = await connect();
-    const [rows] = await conn.query('SELECT * FROM crud.clients;');
-    return rows;
+  const conn = await connect()
+  const [rows] = await conn.query("SELECT * FROM crud.clients;")
+  return rows
 }
 
 async function insertClient(client) {
-    const conn = await connect();
-    const sql = "INSERT INTO crud.clients(name, age, state) VALUES(?, ?, ?);";
-    return await conn.query(sql, [client.name,
-        client.age,
-        client.state
-    ]);
+  const conn = await connect()
+  const sql = "INSERT INTO crud.clients(name, age, state) VALUES(?, ?, ?);"
+  return await conn.query(sql, [client.name, client.age, client.state])
 }
 
 async function selectClient(idclient) {
-    const conn = await connect();
-    const sql = "SELECT * FROM crud.clients WHERE idclient=?";
-    const [rows] = await conn.query(sql, [idclient]);
-    return rows && rows.length > 0 ? rows[0] : {};
+  const conn = await connect()
+  const sql = "SELECT * FROM crud.clients WHERE idclient=?"
+  const [rows] = await conn.query(sql, [idclient])
+  return rows && rows.length > 0 ? rows[0] : {}
 }
 
 async function updateClient(idclient, clients) {
-    const conn = await connect();
-    const sql = "UPDATE crud.clients SET name=?, age=?, state=? WHERE idclient=?";
-    return await conn.query(sql, [
-        clients.name,
-        clients.age,
-        clients.state,
-        idclient
-    ])
+  const conn = await connect()
+  const sql = "UPDATE crud.clients SET name=?, age=?, state=? WHERE idclient=?"
+  return await conn.query(sql, [
+    clients.name,
+    clients.age,
+    clients.state,
+    idclient,
+  ])
 }
 
 async function deleteClient(idclient) {
-    const conn = await connect();
-    return await conn.query('DELETE FROM crud.clients WHERE idclient=?;', [idclient]);
+  const conn = await connect()
+  return await conn.query("DELETE FROM crud.clients WHERE idclient=?;", [
+    idclient,
+  ])
 }
 
-module.exports = {selectClients, insertClient, updateClient, selectClient, deleteClient}
+module.exports = {
+  selectClients,
+  insertClient,
+  updateClient,
+  selectClient,
+  deleteClient,
+}
